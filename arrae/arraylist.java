464398d90b440dda7a9c7a5d4715021f7ecd95a3
Sameer2898 <61672191+Sameer2898@users.noreply.github.com>
2020-10-01 18:26:32 +0530

    Update arraylist.java
    Add some changes

--- arrae/arraylist.java
+++ arrae/arraylist.java
@@ -3,10 +3,10 @@ package arrae;
 //import java.util.ArrayList;
 
 public class arraylist {
-
+	
 	public static void main(String[] args) {
-	  
-//	       Pair<String, Integer> fruits = new Pair("trtr",23);                                                
+	  System.out.println("Arrays Program");
+// 	       Pair<String, Integer> fruits = new Pair("trtr",23);                                                
 		//	syntax ->  ArrayList<type> arraylist = new ArrayList();      
 
 //	       fruits.get();
